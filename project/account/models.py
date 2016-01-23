2c8febc1a71f8182c1ad60cb907002fadcc0f96e
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-23 08:04:17 +0530

    add a profile model,WIP

--- project/account/models.py
+++ project/account/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class Profile(User):
+
+    ext_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
