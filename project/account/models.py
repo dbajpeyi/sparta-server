c68dbfe8ca1360f1e60b67a91e8b0e7406ee38b1
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-25 01:32:54 +0530

    user should be a foreign key instead of being a base class

--- project/account/models.py
+++ project/account/models.py
@@ -8,9 +8,10 @@ from endpoints import GRAVATAR_URL
 
 # Create your models here.
 
-class Profile(User):
+class Profile(models.Model):
 
     ext_id  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user    = models.ForeignKey(User)
     gravatar= models.URLField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
