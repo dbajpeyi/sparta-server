0e1884c5b2c7eed2a7f58737b02d38a917a9b43b
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-25 14:44:27 +0530

    move store settings out to a private file

--- project/article/management/commands/load_articles.py
+++ project/article/management/commands/load_articles.py
@@ -1,8 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+import requests
+import logging
+from article.models import *
+from account.models import *
 
 
 class Command(BaseCommand):
+    
+    SPORTS = ['cricket', 'football']
+    BASE_URL = settings.ARTICLE_URL
 
     def handle(self, *args, **options):
-        print "Load articles"
+        
+        urls = ["%s%s"%(self.BASE_URL, sport) for sport in self.SPORTS]
+        for url in urls:
+            self.create_articles(url)
+
+
 
--- project/settings/sparta.py
+++ project/settings/sparta.py
@@ -1,4 +1,4 @@
 from settings import *
 
-
+ARTICLE_URL='http://indianexpress.com/section/sports/'
 GRAVATAR_URL='http://www.gravatar.com/avatar/'
