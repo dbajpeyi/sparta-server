82807339ac042dab3486ff14fe29127ec5592ff8
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-26 18:45:59 +0530

    change timezone

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -116,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
