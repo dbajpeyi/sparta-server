9cddf02aceb1d466019e6605461b0c551b331a2d
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-26 12:59:23 +0530

    parse articles

--- project/article/management/commands/load_articles.py
+++ project/article/management/commands/load_articles.py
@@ -4,18 +4,44 @@ import requests
 import logging
 from article.models import *
 from account.models import *
+from bs4 import BeautifulSoup
 
 
 class Command(BaseCommand):
     
     SPORTS = ['cricket', 'football']
     BASE_URL = settings.ARTICLE_URL
+    NUM_PAGES = 5
 
     def handle(self, *args, **options):
         
-        urls = ["%s%s"%(self.BASE_URL, sport) for sport in self.SPORTS]
-        for url in urls:
-            self.create_articles(url)
+        logger = logging.getLogger('load_articles')
+        for sport in self.SPORTS:
+            logger.info('setting sport')
+            self.sport = sport
+            obj, created = self.create_sport()
+            self.create_articles(obj)
 
+
+    def create_sport(self):
+        return Sport.objects.get_or_create(name=self.sport, defaults={'name' : self.sport})
+
+    def create_articles(self, sport_obj):
+        for page in xrange(1,self.NUM_PAGES):
+            url = "{0}{1}/page/{2}".format(self.BASE_URL, self.sport, page) 
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            articles = soup.findAll("div", { "class" : "articles" })
+            json_articles = map(self.json_articles, articles)
+            for a in json_articles:
+                print a
+
+    def json_articles(self, article):
+        return {
+            "img" : article.find('img')['src'],
+            "title" : article.find('div', {'class' : 'title'}).find('a').contents[0],
+            "summary" : article.find('p').string.strip(),
+            "posted": article.find('div', {'class' : 'date'}).string.strip() ,
+        }
 
 
--- project/article/views.py
+++ project/article/views.py
@@ -20,3 +20,6 @@ class ArticleList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
