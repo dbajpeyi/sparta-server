231bc7a69cbada97545e6530ab2d2b2fa4ba508e
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-30 16:10:04 +0530

    add pagination to resultsets

--- project/article/views.py
+++ project/article/views.py
@@ -6,9 +6,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from article.serializers import * 
 from article.utils import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class ArticleList(APIView):
+
+    RESULTS_PER_PAGE = 10
 
     def get(self, request, format=None):
         user = get_user_from(request)
@@ -19,16 +22,31 @@ class ArticleList(APIView):
 
     def redis_data_for(self, user):
         if cache.has_key(user.ext_id):
-            return cache.get(user.ext_id)
+            articles = cache.get(user.ext_id)
+            return self.paginate_result(articles)
 
     def get_db_articles_for(self, user):
-        liked_articles = ArticleAction.objects.filter(profile = user, is_liked=True).values_list('article', flat=True)
-        #TODO: In case redis flops, This query needs to only give the latest and the nonliked stuff
-        return Article.objects.exclude(articleaction__article__in = liked_articles)
+        actioned_articles = ArticleAction.objects.filter(profile = user).values_list('article', flat=True)
+        articles = Article.objects.exclude(articleaction__article__in = actioned_articles)
+        return self.paginate_result(articles)
+
+    def paginate_result(self, objects):
+        page = self.request.query_params.get('page')
+        paginator = Paginator(objects, self.RESULTS_PER_PAGE)
+
+        try:
+            objects = paginator.page(page)
+        except PageNotAnInteger:
+            objects = paginator.page(1)
+        except EmptyPage:
+            objects =paginator.page(paginator.num_pages)
+        return objects 
+
                 
 
 
 class ArticleActionView(APIView):
+
 
     def remove_from_redis(self, key, article):
         articles = cache.get(key)
@@ -38,8 +56,8 @@ class ArticleActionView(APIView):
     def post(self, request, format=None):
         user = get_user_from(request)
         article = Article.objects.get(ext_id = request.data.get('ext_id'))
-        self.remove_from_redis(user.ext_id, article)
         is_liked = request.data.get('is_liked')
+        self.remove_from_redis(user.ext_id, article)
         article_action, created = ArticleAction.objects.get_or_create(
                     profile = user,
                     article = article,
